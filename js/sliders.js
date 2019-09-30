2949e34523d5e9e08ab9a1431f0101b638c29fb0
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-09-30 10:43:38 +0300

    Added sliders in the header and shop's section

---
+++ js/sliders.js
@@ -0,0 +1,28 @@
+$(function () {
+  $('.header-slider-wrapper').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    draggable: false,
+    speed: 1000,
+    nextArrow: $('.header-slider__arrow_next'),
+    prevArrow: $('.header-slider__arrow_prev')
+  });
+
+  $('.header-slider__arrow').on('click', function () {
+    let slideIndex = $('.header-slider-wrapper').find('[aria-hidden="false"]')
+      .data('slick-index');
+    $('.header-slider__count-active').text(`${slideIndex + 1}`);
+  });
+});
+
+$(function () {
+  $('.shop-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: $('.shop-slider__arrow_next'),
+    prevArrow: $('.shop-slider__arrow_prev')
+  });
+});
